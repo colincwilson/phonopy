c4b3479a52f3a37ee022ee1e10d48403ead2709e
Colin Wilson <colin.wilson@jhu.edu>
2026-09-07 16:19:08 -0400

    max heapq in alignment

--- phonopy/alignment.py
+++ phonopy/alignment.py
@@ -1,4 +1,3 @@
-# %%
 # Alignment (including self-alignment) of strings or other sequences,
 # possibly weighted by feature similarity.
 # todo: add other self-alignment methods, see wyfst/extra
@@ -17,8 +16,9 @@ from phonopy import (features, strings)
 
 fm = features.read_features()
 
+# # # # # # # # # #
 
-# %%
+
 def adjust_indices(align, offset):
     """
     Adjust the indices of an alignment to account for the fact that the
@@ -49,10 +49,12 @@ def markup_self_alignment(x, align, offset):
     return y
 
 
-def incremental_self_alignment(x, sep=' '):
+def self_align(x, sep=' ', incremental=True):
     """
-    Process each prefix of a string, finding the best alignment of
-    non-overlapping substrings using Smith-Waterman.
+    Find the best alignment of non-overlapping substrings of string/tuple/list 
+    x using the Smith-Waterman algorithm.
+    If incremental is True, process each prefix of the string and return the 
+    best alignment for each prefix.
     """
     if sep != '' and sep is not None:
         x = x.split(sep)
@@ -77,8 +79,14 @@ def incremental_self_alignment(x, sep=' '):
     # Initialize an empty list to store the results
     ret = set()
 
+    # Iterate over prefixes if incremental, else entire string.
+    if incremental:
+        prefix_posns = range(2, xlen + 1)
+    else:
+        prefix_posns = [xlen]
+
     # Iterative over prefixes.
-    for i in range(2, xlen + 1):
+    for i in prefix_posns:
         prefix = x[:i]
         # Find best alignment within a prefix.
         align_best = None
@@ -93,60 +101,62 @@ def incremental_self_alignment(x, sep=' '):
                 align.strings[0], align.strings[1])
             # score /= (xlen / 2) # normalize by string length
             # score /= (i / 2) # normalize by prefix length
-            ret.add((-score, align))
+            ret.add((score, align))
             if score_best is None or score > score_best:
                 score_best = score
                 align_best = align
                 offset_best = j
         #print(align_best, score_best)
         print(
-            f'prefix {i}, {markup_self_alignment(x, align_best, offset_best)}, {-score_best}'
+            f'prefix {i}, {markup_self_alignment(x, align_best, offset_best)}, {score_best}'
         )
 
     ret = list(ret)
-    heapq.heapify(ret)
+    heapq.heapify_max(ret)
     return ret
 
 
-# %%
-examples = [
-    "satu-satuɲa", "kəkaseh-kəkaseh", "sə-səpet", "asal-usol", "llama-llama",
-    "doggy-oggy", "piggy-wiggy", "snalnal", "snalfak", "wofewo"
-]
-x = examples[2]
-x = [x[i] for i in range(len(x)) if x[i] != '-']
-#x = re.sub('[-]', '', x)
-print(x)
-aligns = incremental_self_alignment(x, sep='')
-#pprint(aligns)
+# # # # # # # # # #
 
-# %%
-#x = "s a t u s a t u ɲ a"
-x = 's ə s ə p e t'
-y = x.split(' ')
-y_ = y[:4]
-_y = y[4:]
-print(y_, _y)
+if __name__ == "__main__":
+    examples = [
+        "satu-satuɲa", "kəkaseh-kəkaseh", "sə-səpet", "asal-usol",
+        "llama-llama", "doggy-oggy", "piggy-wiggy", "snalnal", "snalfak",
+        "wofewo"
+    ]
+    x = examples[2]
+    x = [x[i] for i in range(len(x)) if x[i] != '-']
+    #x = re.sub('[-]', '', x)
+    print(x)
+    aligns = self_align(x, sep='')
+    #pprint(aligns)
 
+    #x = "s a t u s a t u ɲ a"
+    x = 's ə s ə p e t'
+    y = x.split(' ')
+    y_ = y[:4]
+    _y = y[4:]
+    print(y_, _y)
 
-def match_func(seg1, seg2):
-    return features.segment_similarity(fm,
-                                       seg1,
-                                       seg2,
-                                       sim_func=features.overlap_similarity,
-                                       feature_weights=None,
-                                       offset=-0.75,
-                                       indel_weight=-1.0)
+    def match_func(seg1, seg2):
+        return features.segment_similarity(
+            fm,
+            seg1,
+            seg2,
+            sim_func=features.overlap_similarity,
+            feature_weights=None,
+            offset=-0.75,
+            indel_weight=-1.0)
 
+    aligner = SmithWaterman(match_func=match_func,
+                            gap_char=phon_config.epsilon)
+    align = aligner.get_alignment(y_,
+                                  _y,
+                                  return_indices=True,
+                                  return_score_matrix=True)
+    print(len(align))
+    pprint(align)
 
-aligner = SmithWaterman(match_func=match_func, gap_char=phon_config.epsilon)
-align = aligner.get_alignment(y_,
-                              _y,
-                              return_indices=True,
-                              return_score_matrix=True)
-print(len(align))
-pprint(align)
-
-print(align.score_matrix)
-plt.imshow(align.score_matrix)
-plt.show()
+    print(align.score_matrix)
+    plt.imshow(align.score_matrix)
+    plt.show()
